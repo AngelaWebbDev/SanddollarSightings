143085f89a4b9bb699d20bdf848210902508b9db
Angela Webb <angelawebb.dev@gmail.com>
2024-04-22 05:50:20 -0400

    adds routes for registration/login

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,13 @@
 const UserController = require('../controllers/user.controller')
 
 module.exports = (app) => {
-    //create
-    app.post('/api/newuser',UserController.addUser);
+    
+    //create new user
+    app.post('/api/newuser',UserController.registerUser);
+
+    //login with cookie
+    app.post('/api/login', UserController.login)
+
+    //log out, clear cookie
+    app.post('/api/logout', UserController.logout)
 }
